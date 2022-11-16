fea5b0ca8be2f2ca96dedf4517f649fd9397740b
dkfjfdjdj <57519838+dkfjfdjdj@users.noreply.github.com>
2022-11-16 17:05:56 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var t1=document.getElementById("tier_begin");
+var t2=document.getElementById("tier_end");
+var s1=document.getElementById("solvers_begin");
+var s2=document.getElementById("solvers_end");
+var is_checked=document.getElementById("byme").checked;
+var ref_str=document.getElementsByName("ref");
+var ref=ref_str.options[ref_str.selectedIndex].value;
